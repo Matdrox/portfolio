bd4c13368ada71faeb443c3932363c77db7aabe8
Matei Cananau <mateicananau@gmail.com>
2022-07-04 20:15:18 +0200

    Updated TapDasher Showcase & Translated

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -2,9 +2,11 @@ import { useState, useRef, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import imagesHellenic from './imgComps/ImagesHellenic';
 import imagesSphere from './imgComps/ImagesSphere';
+import imagesTapDasher from './imgComps/imagesTapDasher';
 import Button from './buttons/Button';
-import { FaReact, FaDatabase } from 'react-icons/fa';
-import { SiTailwindcss, SiPython } from 'react-icons/si';
+import { FaReact, FaDatabase, FaUnity } from 'react-icons/fa';
+import { DiApple, DiAndroid } from 'react-icons/di';
+import { SiTailwindcss, SiPython, SiCsharp } from 'react-icons/si';
 import { TbApi, TbMathSymbols } from 'react-icons/tb';
 import { BsDisplay } from 'react-icons/bs';
 
@@ -237,59 +239,61 @@ const Page2 = ({ fontColor, lang, boxColor }) => {
 				whileInView={{ opacity: 1 }}
 				viewport={{ once: false }}
 			>
-				<h1 className='text-3xl'>
-					{lang ? 'MATHEMATICAL SPHERE' : 'SPHERA MATHEMATIC'}
-				</h1>
+				<h1 className='text-3xl'>{lang ? 'TAPDASHER' : 'TAPDASHER'}</h1>
 				<br />
 				<p>
 					{lang
-						? 'Python application that displays a sphere along with its shadow using the dot product.'
-						: 'Application de Python que monstrar un sphera con su umbra usante le producto scalare.'}
+						? 'Unity game using C# publishd by 17 year old me on the App Store and Google Play.'
+						: 'Joco Unity usante C# publicava a me de 17 annos por le App Store e Google Play.'}
 					<br />
 					{lang
-						? "The user enters the lightsource's position and the program outputs a mathematically correct sphere."
-						: 'Le usator inscriber le position del origine del lumine e le programma monstrar un sphera correcte mathematic.'}
+						? "The player's character automatically moves diagonally to the right but moves to the left when the screen is."
+						: 'Le character del jocator se move automatic diagonal a dextra ma a sinistra quando le schermo es tacto.'}
 					<br />
 					{lang
-						? 'It is also posible to click anywhere on the sphere to move the light source, updating the sphere.'
-						: 'Es possibile clickar ubique al le sphera por movente le origine del lumine e le sphera con su umbra.'}
+						? 'It is possible buying skins for the character from the menu with the points gathered while playing.'
+						: 'Es possibile comprar costumes por le character ab le menu con le punctos colliger durante que joca.'}
 					<br />
 					{lang
-						? 'The application allows the user to save the displayed sphere to a text file.'
-						: 'Le application permitter le usator sparniar le sphere a un fila de texto.'}
+						? 'The app uses a scoreboard to let friends compete with each other.'
+						: 'Le application usante un tabula de puncto a lassar amicos concurrer con inter se.'}
 				</p>
 				<br />
 				<h2 className='text-xl'>
 					{lang ? 'TECHNOLOGIES USED' : 'TECHNOLOGIA USAVA'}
 				</h2>
 				<div className='flex flex-row'>
-					<div className='flex flex-col justify-center items-center group mt-1 -mx-6 z-10'>
-						<SiPython className='text-2xl text-purple-600 transition group-hover:text-orange-400' />
+					<div className='flex flex-col justify-center items-center group mt-1'>
+						<FaUnity className='text-2xl text-purple-600 transition group-hover:text-orange-400' />
 						<div className='bg-zinc-300 rounded text-black text-center text-sm flex items-center justify-center transition mt-1 duration-300 opacity-0 group-hover:opacity-100'>
-							Python
+							Unity
 						</div>
 					</div>
-					<div className='flex flex-col justify-center items-center group -mx-2'>
-						<TbMathSymbols className='text-3xl text-purple-600 transition group-hover:text-orange-400' />
+					<div className='flex flex-col justify-center items-center group'>
+						<SiCsharp className='text-3xl text-purple-600 transition group-hover:text-orange-400' />
 						<div className='bg-zinc-300 rounded text-black text-center text-sm flex items-center justify-center mt-1 transition duration-300 opacity-0 group-hover:opacity-100'>
-							{lang ? 'Pure Maths' : 'Mathematica Pur'}
+							C#
 						</div>
 					</div>
-					<div className='flex flex-col justify-center items-center group -mx-2 z-10'>
-						<BsDisplay className='text-3xl text-purple-600 transition group-hover:text-orange-400' />
+					<div className='flex flex-col justify-center items-center group'>
+						<DiApple className='text-3xl text-purple-600 transition group-hover:text-orange-400' />
 						<div className='bg-zinc-300 rounded text-black text-center text-sm flex items-center justify-center mt-1 transition duration-300 opacity-0 group-hover:opacity-100'>
-							GUI
+							Apple
+						</div>
+					</div>
+					<div className='flex flex-col justify-center items-center group -mx-3'>
+						<DiAndroid className='text-3xl text-purple-600 transition group-hover:text-orange-400' />
+						<div className='bg-zinc-300 rounded text-black text-center text-sm flex items-center justify-center mt-1 transition duration-300 opacity-0 group-hover:opacity-100'>
+							Android
 						</div>
 					</div>
 				</div>
 				<a
 					className='underline'
-					href='https://github.com/Matdrox/sphere'
+					href='https://play.google.com/store/apps/details?id=com.MatdroxGames.TapDash'
 					target='_blank'
 				>
-					<h2 className='text-md'>
-						{lang ? 'SOURCE CODE' : 'LE CODICE ORIGINAL'}
-					</h2>
+					<h2 className='text-md'>GOOGLE PLAY</h2>
 				</a>
 			</motion.div>
 			<motion.div
@@ -306,10 +310,10 @@ const Page2 = ({ fontColor, lang, boxColor }) => {
 					dragConstraints={{ right: 0, left: -width }}
 					className='flex'
 				>
-					{imagesSphere.map((image) => {
+					{imagesTapDasher.map((image) => {
 						return (
 							<motion.div
-								className='h-[600px] min-w-[33em] py-4 px-2'
+								className='h-[500px] min-w-[33em] py-4 px-2'
 								key={image}
 							>
 								<img
---
+++ src/components/imgComps/imagesTapDasher.js
@@ -0,0 +1,6 @@
+import image1 from '../../img/imgTapDasher1.png';
+import image2 from '../../img/imgTapDasher2.png';
+import image3 from '../../img/imgTapDasher3.png';
+import image4 from '../../img/imgTapDasher4.png';
+
+export default [image1, image2, image3, image4];
