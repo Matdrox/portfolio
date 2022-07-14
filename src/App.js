b6423f56643cd127c5fb1123dbe5fa8b4845b069
Matei Cananau <mateicananau@gmail.com>
2022-07-14 15:28:54 +0200

    Implemented Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
@@ -6,37 +7,43 @@ import Switch from './components/buttons/Switch';
 import LangButton from './components/buttons/LangButton';
 
 function App() {
-  const lightTheme = 'bg-zinc-100';
-  const darkTheme = 'bg-zinc-800';
+	useEffect(() => {
+		ReactGA.initialize('G-EMFTQMKQ7J');
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
 
-  const lightBox = 'bg-white';
-  const darkBox = 'bg-zinc-900';
+	const lightTheme = 'bg-zinc-100';
+	const darkTheme = 'bg-zinc-800';
 
-  const lightFont = 'text-zinc-800';
-  const darkFont = 'text-zinc-200';
+	const lightBox = 'bg-white';
+	const darkBox = 'bg-zinc-900';
 
-  const [enabled, setEnabled] = useState(false);
-  const [lang, setLang] = useState('english');
-  
-  const fontColor = enabled ? darkFont : lightFont;
-  const boxColor = enabled ? darkBox : lightBox;
-  return (
-    <div
-      className={`transition ease-in-out duration-500 ${
-        enabled ? darkTheme : lightTheme
-      }`}
-    >
-      <Switch
-        enabled={enabled}
-        setEnabled={setEnabled}
-        onClick={() => setEnabled(!enabled)}
-      />
-      <LangButton text={lang ? 'EN' : 'IA'} onClick={() => setLang(!lang)} />
-      <Page1 fontColor={fontColor} lang={lang} />
-      <Page2 fontColor={fontColor} lang={lang} boxColor={boxColor}/>
-      <Page3 fontColor={fontColor} lang={lang} />
-    </div>
-  );
+	const lightFont = 'text-zinc-800';
+	const darkFont = 'text-zinc-200';
+
+	const [enabled, setEnabled] = useState(false);
+	const [lang, setLang] = useState('english');
+
+	const fontColor = enabled ? darkFont : lightFont;
+	const boxColor = enabled ? darkBox : lightBox;
+
+	return (
+		<div
+			className={`transition ease-in-out duration-500 ${
+				enabled ? darkTheme : lightTheme
+			}`}
+		>
+			<Switch
+				enabled={enabled}
+				setEnabled={setEnabled}
+				onClick={() => setEnabled(!enabled)}
+			/>
+			<LangButton text={lang ? 'EN' : 'IA'} onClick={() => setLang(!lang)} />
+			<Page1 fontColor={fontColor} lang={lang} />
+			<Page2 fontColor={fontColor} lang={lang} boxColor={boxColor} />
+			<Page3 fontColor={fontColor} lang={lang} />
+		</div>
+	);
 }
 
 export default App;
