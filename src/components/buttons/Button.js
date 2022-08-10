46d66fafef2959c5035f9d73ca5deeeab55ecea0
Matei Cananau <mateicananau@gmail.com>
2022-08-10 23:40:38 +0200

    Cleaned Up Code

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -1,23 +1,20 @@
 import { motion } from 'framer-motion';
 
 const Button = (props) => {
-  return (
-    <button
-      // className={`bg-purple-600 ${props.width} ${props.height} rounded-lg shadow-lg shadow-purple-600/30 flex items-center justify-center
-      // transition ease-in-out duration-400
-      // hover:bg-orange-400 hover:shadow-orange-400/30`}
-      className={`bg-purple-600 w-36 h-14 rounded-lg shadow-lg shadow-purple-600/30 flex items-center justify-center
+	return (
+		<button
+			className={`bg-purple-600 w-36 h-14 rounded-lg shadow-lg shadow-purple-600/30 flex items-center justify-center
       transition ease-in-out duration-400 
       hover:bg-orange-400 hover:shadow-orange-400/30`}
-      style={{
-        width: `${props.width}`,
-        height: `${props.height}`,
-      }}
-      onClick={props.onClick}
-    >
-      <p className='text-white pointer-events-none'>{props.text}</p>
-    </button>
-  );
+			style={{
+				width: `${props.width}`,
+				height: `${props.height}`,
+			}}
+			onClick={props.onClick}
+		>
+			<p className='text-white pointer-events-none'>{props.text}</p>
+		</button>
+	);
 };
 
 export default Button;
--- src/components/buttons/Switch.js
+++ src/components/buttons/Switch.js
@@ -1,32 +1,35 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-// import { motion, useMotionValue, useTransform } from 'framer-motion';
 
 const Switch = (props) => {
-  // STORE THEME LOCALLY!
-  const toggleSwitch = () => props.setEnabled(!props.enabled);
+	// STORE THEME LOCALLY!!!!!
+	const toggleSwitch = () => props.setEnabled(!props.enabled);
 
-  const spring = {
-    type: 'spring',
-    stiffness: 700,
-    damping: 30,
-  };
+	const spring = {
+		type: 'spring',
+		stiffness: 700,
+		damping: 30,
+	};
 
-  return (
-    <div
-      onClick={toggleSwitch}
-      className={`${props.enabled ? 'bg-purple-600 shadow-lg shadow-purple-600/30' : 'bg-orange-400 shadow-lg shadow-orange-400/30'} transition duration-300 flex-start flex h-[45px] w-[80px] p-1 absolute top-0 left-12 m-10 rounded-full
+	return (
+		<div
+			onClick={toggleSwitch}
+			className={`${
+				props.enabled
+					? 'bg-purple-600 shadow-lg shadow-purple-600/30'
+					: 'bg-orange-400 shadow-lg shadow-orange-400/30'
+			} transition duration-300 flex-start flex h-[45px] w-[80px] p-1 absolute top-0 left-12 m-10 rounded-full
     hover:cursor-pointer md:left-auto md:right-0 ${
-      props.enabled && 'place-content-end'
-    }`}
-    >
-      <motion.div
-        className='flex h-[39px] w-[40px] absolute top-[3px] items-center justify-center rounded-full bg-white'
-        layout
-        transition={spring}
-      ></motion.div>
-    </div>
-  );
+			props.enabled && 'place-content-end'
+		}`}
+		>
+			<motion.div
+				className='flex h-[39px] w-[40px] absolute top-[3px] items-center justify-center rounded-full bg-white'
+				layout
+				transition={spring}
+			></motion.div>
+		</div>
+	);
 };
 
 export default Switch;
