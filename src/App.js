4d35fa7d83a81720b2d8cb9e847268c695dd9900
Matdrox <Matdrox@users.noreply.github.com>
2022-09-16 11:29:18 +0200

    [Updated] Show Language

--- src/App.js
+++ src/App.js
@@ -5,45 +5,50 @@ import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 import Switch from './components/buttons/Switch';
 import LangButton from './components/buttons/LangButton';
+import imgArrow from './img/arrow.png';
 
 function App() {
-	useEffect(() => {
-		ReactGA.initialize('UA-235238761-1');
-		ReactGA.pageview(window.location.pathname + window.location.search);
-	}, []);
+  useEffect(() => {
+    ReactGA.initialize('UA-235238761-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
-	const lightTheme = 'bg-zinc-100';
-	const darkTheme = 'bg-zinc-800';
+  const lightTheme = 'bg-zinc-100';
+  const darkTheme = 'bg-zinc-800';
 
-	const lightBox = 'bg-white';
-	const darkBox = 'bg-zinc-700';
+  const lightBox = 'bg-white';
+  const darkBox = 'bg-zinc-700';
 
-	const lightFont = 'text-zinc-800';
-	const darkFont = 'text-zinc-200';
+  const lightFont = 'text-zinc-800';
+  const darkFont = 'text-zinc-200';
 
-	const [enabled, setEnabled] = useState(false);
-	const [lang, setLang] = useState('english');
+  const [enabled, setEnabled] = useState(false);
+  const [lang, setLang] = useState('english');
 
-	const fontColor = enabled ? darkFont : lightFont;
-	const boxColor = enabled ? darkBox : lightBox;
+  const fontColor = enabled ? darkFont : lightFont;
+  const boxColor = enabled ? darkBox : lightBox;
 
-	return (
-		<div
-			className={`transition ease-in-out duration-500 ${
-				enabled ? darkTheme : lightTheme
-			}`}
-		>
-			<Switch
-				enabled={enabled}
-				setEnabled={setEnabled}
-				onClick={() => setEnabled(!enabled)}
-			/>
-			<LangButton text={lang ? 'EN' : 'IA'} onClick={() => setLang(!lang)} />
-			<Page1 fontColor={fontColor} lang={lang} />
-			<Page2 fontColor={fontColor} lang={lang} boxColor={boxColor} />
-			<Page3 fontColor={fontColor} lang={lang} boxColor={boxColor} />
-		</div>
-	);
+  return (
+    <div
+      className={`transition ease-in-out duration-500 ${
+        enabled ? darkTheme : lightTheme
+      }`}
+    >
+      <Switch
+        enabled={enabled}
+        setEnabled={setEnabled}
+        onClick={() => setEnabled(!enabled)}
+      />
+      <LangButton text={lang ? 'EN' : 'IA'} onClick={() => setLang(!lang)} />
+      <div className='text-zinc-400 top-[40px] right-20 m-10 absolute font-serif font-homemade'>
+        <img src={imgArrow} />
+        <p className='absolute mr-8 mt-2'>{lang ? 'english' : 'interlingua'}</p>
+      </div>
+      <Page1 fontColor={fontColor} lang={lang} />
+      <Page2 fontColor={fontColor} lang={lang} boxColor={boxColor} />
+      <Page3 fontColor={fontColor} lang={lang} boxColor={boxColor} />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      'homemade': ['Homemade Apple', 'cursive']
+    },
     extend: {
       visibility: ["group-hover"],
     },
